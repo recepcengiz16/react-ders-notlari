68f6294bfcfd8523f1588878ebd5f6fae3c09b27
Recep <recepcengiz_96@outlook.com>
2023-12-25 17:13:53 +0300

    props yöntemi ile veri gösterilmesi yapıldı

--- 2023-12-25/jsx-createelement-fragment/src/App.js
+++ 2023-12-25/jsx-createelement-fragment/src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import {Fragment, createElement} from "react";
 
+function Button(props){
+  return <button>{ props.text }</button>
+}
 
 function App() {
 
   const todos =["todo1","todo2","todo3"];
+
+  // <Fragment>
+        //Fragment nedir => normalde html taglerini bir root elementin sarmalaması lazım. Ama ben bir root elementin sarmalamasını istemiyorum. root elementi yerine fragment geldi. Bunun içinde yazınca sanki sarmalıyor gibi ama sarmalamamış şekilde kod çıktısı veriyor
+
+      //  <></> bu da fragmentın kısa yolu boş bir şekilde
+  // </Fragment>
   
   return (
-
-    // <Fragment>
-          //Fragment nedir => normalde html taglerini bir root elementin sarmalaması lazım. Ama ben bir root elementin sarmalamasını istemiyorum. root elementi yerine fragment geldi. Bunun içinde yazınca sanki sarmalıyor gibi ama sarmalamamış şekilde kod çıktısı veriyor
-
-        //  <></> bu da fragmentın kısa yolu boş bir şekilde
-    // </Fragment>
 
     <main>
       <ul>
@@ -21,19 +24,20 @@ function App() {
          ))}
       </ul>
 
-      <h1 style={{color:'red',backgroundColor:'yellow'}}>Denemelere devam</h1> style verirken {{  }} süslü parantezin içinde vermek lazım style ayarlarını
+      <h1 style={{color:'red',backgroundColor:'yellow'}}>Denemelere devam</h1> 
+      {/* style verirken {{}} süslü parantezin içinde vermek lazım style ayarlarını */}
 
-      <label for="focus">
+      <label htmlFor="focus">
         <input type='text' id='focus'/> Odaklanması için labela tıklayınca inputa focus yapıyor ya onun yerine htmlFor da yazabilirsin
       </label>   
           
       {'Bu örnekte, <label> elementinin "htmlFor" özelliği, "myInput" olarak ayarlanmış ve aynı "id" değeri, <input> elementinde de kullanılmıştır. Böylece "htmlFor" ile etiketlenen <label> elementine tıklandığında, <input> elementine odaklanılacaktır. Bu, özellikle erişilebilirlik (accessibility) açısından önemlidir, çünkü bu şekilde ekran okuyucular gibi araçlar doğru şekilde etiketlenmiş form elemanlarına odaklanabilir.'}
 
-      {} js ifadesi yazabilirsin süslü parantezin içerisinde
+      {/* {} js ifadesi yazabilirsin süslü parantezin içerisinde */}
 
-      <input type="text" onClick={alert("Bana el salla")} /> html taginde js fonks. kullanırken ilkharfi küçük sonraki harfi büyük olacak şekilde oluyor. 
+      <input type="text" onClick={() => alert("Bana el salla")} /> html taginde js fonks. kullanırken ilkharfi küçük sonraki harfi büyük olacak şekilde oluyor. 
 
-    
+      <Button text="Merhaba Recep"></Button>
 
     </main>
   )
