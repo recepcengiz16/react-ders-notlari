3e16cb26018315949c9137bec90eb66fd93c7bd5
Recep <recepcengiz_96@outlook.com>
2023-12-25 13:33:21 +0300

    liste içerisinde dönme

--- 2023-12-25/jsx-createelement-fragment/src/App.js
+++ 2023-12-25/jsx-createelement-fragment/src/App.js
@@ -4,12 +4,23 @@ import {createElement} from "react";
 
 function App() {
 
-  const h1=createElement("h1",null,"h1 elementi");
-
-  return createElement("main",{    
-    id:"main",
-    className:"test"
-  },h1);
+  const todos =["todo1","todo2","todo3"];
+  
+  return (
+    <main>
+      <ul>
+         {todos.forEach(x=>(
+          <li>{x}</li>
+         ))}
+      </ul>
+    </main>
+  )
+  
+  // const h1=createElement("h1",null,"h1 elementi");
+  // return createElement("main",{    
+  //   id:"main",
+  //   className:"test"
+  // },h1);
 
   // return createElement("main",{
       
